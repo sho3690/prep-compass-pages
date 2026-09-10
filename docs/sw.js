64fc0187d6f1c33738fde862dae0ffe1f98599d0
prep-compass <prep-compass@local>
2026-09-10 14:46:28 +0900

    update 2026-09-10 14:46

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "prep-compass-v1";
+const CACHE = "sakiyomi-v2";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { e.waitUntil(self.clients.claim()); });
 self.addEventListener("fetch", (e) => {
